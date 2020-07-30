e5019bb42375de96d00284e52af4d1d7227aa551
Daniel Weber <daniel.weber@uni-tuebingen.de>
2020-07-30 19:19:03 +0200

    Consider the thickness of the plate (in the mount) on which QR Code is printed

--- Assets/Scripts/QRCodeTarget.cs
+++ Assets/Scripts/QRCodeTarget.cs
@@ -9,12 +9,17 @@ public class QRCodeTarget : MonoBehaviour
     [Tooltip("Content of the QR Code to be aligned with.")]
     private string QRCodeContent = "Uni Tuebingen";
 
+    [SerializeField]
+    [Tooltip("Thickness of the board or paper in the mount on which the QR code is printed [in mm].")]
+    private float boardThickness = 5;
+
     private GameObject qrCodeObject = null;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        boardThickness /= 1000;  // mm -> m (Unity units)
+        gameObject.transform.Find("Visuals").transform.localPosition -= new Vector3(0, 0, boardThickness);
     }
 
     // Update is called once per frame
