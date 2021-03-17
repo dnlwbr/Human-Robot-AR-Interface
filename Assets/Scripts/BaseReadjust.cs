bebfe3061767bae8fdd1210628222c02eb3edae7
Daniel Weber <daniel.weber@uni-tuebingen.de>
2021-03-17 20:10:56 +0100

    Adds draft for readjustment of base (added to scene but parts of the script are commented)

---
+++ Assets/Scripts/BaseReadjust.cs
@@ -0,0 +1,64 @@
+﻿using Microsoft.MixedReality.Toolkit.UI;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace HumanRobotInterface
+{
+    public class BaseReadjust : MonoBehaviour
+    {
+        [SerializeField]
+        private MarkerCalibration calibrationMarker;
+
+        [SerializeField]
+        [Tooltip("(Optional) Gameobject that visualizes trajectory.")]
+        private GameObject footprintVisualization = null;
+
+        [SerializeField]
+        [Tooltip("Button that triggers activation to untoggled it if necessary.")]
+        private Interactable interactable;
+
+        private Vector3 translation;
+        private Quaternion rotation;
+
+
+        void OnEnable()
+        {
+            if (calibrationMarker.isCalibrated)
+            {
+                // Get TF's translation in the frame of calibrationMarker
+                translation = calibrationMarker.transform.InverseTransformVector(transform.TransformVector(-calibrationMarker.base_footprint2Kinect.position));
+
+                // Get rotation from marker to base_footprint
+                rotation = transform.rotation * Quaternion.Inverse(calibrationMarker.transform.rotation);
+
+                if (footprintVisualization)
+                {
+                    footprintVisualization.SetActive(true);
+                }
+            }
+            else
+            {
+                //interactable.SetState(InteractableStates.InteractableStateEnum.Toggled, false);
+            }
+        }
+
+        void OnDisable()
+        {
+            if (footprintVisualization)
+            {
+                footprintVisualization.SetActive(false);
+            }
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+            /*
+            transform.position = calibrationMarker.transform.TransformPoint(translation);
+            transform.rotation = rotation * calibrationMarker.transform.rotation;
+            calibrationMarker.transform.localPosition = calibrationMarker.base_footprint2Kinect.position;
+            */
+        }
+    }
+}
